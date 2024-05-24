faec11a80cb183f5c220f6588d030955e06bd12f
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-05-24 14:49:44 +0000

    added Cards + Top Nav by Akash

--- my-project/src/components/Cards/Cards.jsx
+++ my-project/src/components/Cards/Cards.jsx
@@ -8,18 +8,21 @@ function Cards() {
 
   return (
     <>
+  
   <div className="container">
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-    alt="Laptop"
+    src="https://images.pexels.com/photos/1181568/pexels-photo-1181568.jpeg?auto=compress&cs=tinysrgb&w=600"
+    alt="Error"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship A</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      3 months <br />
+      Mumbai <br />
+      Work from Home <br />
+      Starts immediately <br />
     </p>
     <button
       type="button"
@@ -32,15 +35,17 @@ function Cards() {
 </div>
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    src="https://images.pexels.com/photos/6476186/pexels-photo-6476186.jpeg?auto=compress&cs=tinysrgb&w=600"
     alt="Laptop"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship B</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      6 months <br />
+      Navi Mumbai <br />
+      Work from Home <br />
+      Starts immediately <br />
     </p>
     <button
       type="button"
@@ -53,15 +58,17 @@ function Cards() {
 </div>
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuGvsKI9do6CvYNIdztB1dLuUUGGgy-eLtaw&s"
     alt="Laptop"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship C</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      2 months <br />
+      Hyderabad <br />
+      Work from Home <br />
+      Starts immediately <br />
     </p>
     <button
       type="button"
@@ -74,15 +81,17 @@ function Cards() {
 </div>
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-4Lo8XU5YSQ2jzo5eKd8ajjWQ17eilCHKgA&s"
     alt="Laptop"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship D</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      2 months <br />
+      Delhi <br />
+      Work from Home <br />
+      Starts immediately <br />
     </p>
     <button
       type="button"
@@ -95,15 +104,16 @@ function Cards() {
 </div>
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    src="https://images.pexels.com/photos/3747455/pexels-photo-3747455.jpeg?auto=compress&cs=tinysrgb&w=600"
     alt="Laptop"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship E</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      4 months <br />
+      Mumbai <br />
+      In office <br />
     </p>
     <button
       type="button"
@@ -116,15 +126,16 @@ function Cards() {
 </div>
     <div class=" w-[300px] rounded-md border">
   <img
-    src="https://images.pexels.com/photos/23881778/pexels-photo-23881778/free-photo-of-on-a-cloudy-day-at-the-lakefront-of-keswick-england.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc-rSo6KDmN6hZp4o3Isvrn9it8RQODvy9cw&s"
     alt="Laptop"
     class="h-[200px] w-full rounded-md object-cover"
   />
   <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
+    <h1 class="text-lg font-semibold">Internship F</h1>
     <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+      3 months <br />
+      Pune <br />
+      In office <br />
     </p>
     <button
       type="button"
@@ -135,9 +146,9 @@ function Cards() {
     </button>
   </div>
 </div>
+
 </div>
     
-
     </>
   )
 }
