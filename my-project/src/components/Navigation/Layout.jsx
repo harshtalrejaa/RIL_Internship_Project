5b0485c8451fd052941237f807444a3ecf299d1a
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-05-26 09:06:24 +0000

    TopNav by Akash

--- my-project/src/components/Navigation/Layout.jsx
+++ my-project/src/components/Navigation/Layout.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SignButton from './SignButton';
+import TopNav from './TopNav';
 
 const Layout = () => {
   return (
     <div className="layout">
       <div className="main-content">
+        <TopNav/>
         <SignButton/>
         <Outlet />
       </div>
---
+++ my-project/src/components/Navigation/TopNav.jsx
@@ -0,0 +1,23 @@
+// import './index.css';
+import React from 'react'
+
+function TopNav() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <div class="left">Jio</div>
+                    <div class="right">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">About Company</a></li>
+                            <li><a href="/">Programs</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default TopNav
