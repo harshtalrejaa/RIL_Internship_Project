f8b22857c33e985bc5aadcbac40650899d506151
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-05-26 16:39:08 +0000

    text change

--- my-project/src/components/FormBuilder/FormBuilder.jsx
+++ my-project/src/components/FormBuilder/FormBuilder.jsx
@@ -47,14 +47,14 @@ function FormBuilder() {
   }
 
   return (
-    <form className="text-white-900" onSubmit={submit}>
+    <form className="text-black-900" onSubmit={submit}>
       <div className="border-b border-gray-900/10 pb-12">
-        <h2 className="text-base font-semibold leading-7 text-white-900">Personal Information</h2>
-        <p className="mt-1 text-sm leading-6 text-white-600">Use a permanent address where you can receive mail.</p>
+        <h2 className="text-base font-semibold leading-7 text-black-900">Personal Information</h2>
+        <p className="mt-1 text-sm leading-6 text-black-600">Use a permanent address where you can receive mail.</p>
 
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-3">
-            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-white-900">First name</label>
+            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-black-900">First name</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -63,13 +63,13 @@ function FormBuilder() {
                 autoComplete="given-name"
                 value={formData.firstName}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-3">
-            <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-white-900">Last name</label>
+            <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-black-900">Last name</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -78,13 +78,13 @@ function FormBuilder() {
                 autoComplete="family-name"
                 value={formData.lastName}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-4">
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-white-900">Email address</label>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-black-900">Email address</label>
             <div className="mt-2">
               <input
                 id="email"
@@ -93,13 +93,13 @@ function FormBuilder() {
                 autoComplete="email"
                 value={formData.email}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-3">
-            <label htmlFor="country" className="block text-sm font-medium leading-6 text-white-900">Country</label>
+            <label htmlFor="country" className="block text-sm font-medium leading-6 text-black-900">Country</label>
             <div className="mt-2">
               <select
                 id="country"
@@ -107,7 +107,7 @@ function FormBuilder() {
                 autoComplete="country-name"
                 value={formData.country}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
               >
                 <option>India</option>
                 <option>Canada</option>
@@ -117,7 +117,7 @@ function FormBuilder() {
           </div>
 
           <div className="col-span-full">
-            <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-white-900">Street address</label>
+            <label htmlFor="street-address" className="block text-sm font-medium leading-6text-black-900">Street address</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -126,13 +126,13 @@ function FormBuilder() {
                 autoComplete="street-address"
                 value={formData.streetAddress}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-2 sm:col-start-1">
-            <label htmlFor="city" className="block text-sm font-medium leading-6 text-white-900">City</label>
+            <label htmlFor="city" className="block text-sm font-medium leading-6 text-black-900">City</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -141,13 +141,13 @@ function FormBuilder() {
                 autoComplete="address-level2"
                 value={formData.city}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-2">
-            <label htmlFor="region" className="block text-sm font-medium leading-6 text-white-900">State / Province</label>
+            <label htmlFor="region" className="block text-sm font-medium leading-6 text-black-900">State / Province</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -156,13 +156,13 @@ function FormBuilder() {
                 autoComplete="address-level1"
                 value={formData.region}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
           <div className="sm:col-span-2">
-            <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-white-900">ZIP / Postal code</label>
+            <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-black-900">ZIP / Postal code</label>
             <div className="mt-2">
               <input
                 type="text"
@@ -171,15 +171,15 @@ function FormBuilder() {
                 autoComplete="postal-code"
                 value={formData.postalCode}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
         </div>
       </div>
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button onClick={cancel} type="button" className="text-sm font-semibold leading-6 text-white-900">Cancel</button>
-        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        <button onClick={cancel} type="button" className="text-sm font-semibold leading-6 text-black-900">Cancel</button>
+        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
       </div>
     </form>
   );
