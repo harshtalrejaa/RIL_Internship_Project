5f6fff0e96cccb668c2a1fff034cc2a6b625c984
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-06-01 09:11:20 +0000

    Changes in Form by Akash

--- my-project/src/components/FormBuilder/FormBuilder.jsx
+++ my-project/src/components/FormBuilder/FormBuilder.jsx
@@ -51,7 +51,7 @@ function FormBuilder() {
       <form className="text-black-900" onSubmit={submit}>
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-black">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-black">Use a permanent address where you can receive mail.</p>
+          {/* <p className="mt-1 text-sm leading-6 text-black">Use a permanent address where you can receive mail.</p> */}
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -136,23 +136,9 @@ function FormBuilder() {
               </div>
             </div>
 
-            <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-black">Address</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="streetAddress"
-                  id="street-address"
-                  autoComplete="street-address"
-                  value={formData.streetAddress}
-                  onChange={handleChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
 
             <div className="sm:col-span-2 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 text-black">City</label>
+              <label htmlFor="city" className="block text-sm font-medium leading-6 text-black">Employee ID</label>
               <div className="mt-2">
                 <input
                   type="text"
@@ -181,20 +167,6 @@ function FormBuilder() {
               </div>
             </div>
 
-            <div className="sm:col-span-2">
-              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-black">Link to Resume</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="postalCode"
-                  id="postal-code"
-                  autoComplete="postal-code"
-                  value={formData.postalCode}
-                  onChange={handleChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-black-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
           </div>
         </div>
         <div className="mt-6 flex items-center justify-end gap-x-6">
