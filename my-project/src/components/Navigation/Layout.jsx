8c539f7ccc8b3840f241ba5a6feef988c2e2cf0e
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-06-06 14:55:59 +0000

    Error solving by Akash

--- my-project/src/components/Navigation/Layout.jsx
+++ my-project/src/components/Navigation/Layout.jsx
@@ -4,14 +4,24 @@ import { Outlet } from 'react-router-dom';
 import SignButton from './SignButton';
 import TopNav from './TopNav';
 import Footer from './Footer';
+import Hero from '../Hero/Hero';
+import Title from '../Title/Title';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
 
 const Layout = () => {
   return (
     <div className="layout">
       <div className="main-content">
-        <TopNav/>
+      <TopNav/>
+        <Hero/>
+        <Title subTitle="Get to know company" title="About Us" />
         <SignButton/>
+        <About/>
+        <Title subTitle="Apply here" title="Postings" />
         <Outlet />
+        <Title subTitle="Contact Us" title="Get In Touch" />
+        <Contact/>
         <Footer/>
       </div>
     </div>
