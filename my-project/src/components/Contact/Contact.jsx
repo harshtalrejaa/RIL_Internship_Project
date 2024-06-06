1d913f63dd64aed9be8f00db298fa447cdc9de26
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-06-06 14:43:22 +0000

    Contact, Main page by Akash

---
+++ my-project/src/components/Contact/Contact.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "35d3c6ce-09c7-4187-937d-37ad0b26b892");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      console.log("Success", data);
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            {" "}
+            <img src={mail_icon} alt="" />
+            Jio.NetworkAcademy@ril.com
+          </li>
+          <li>
+            {" "}
+            <img src={phone_icon} alt="" />
+            022 3555 5000
+          </li>
+          <li>
+            {" "}
+            <img src={location_icon} alt="" />
+            Reliance Corporate Park, Elastomer<br/> Business Group, 8A-FF,Thane Belapur Road, <br/> Ghansoli, Navi Mumbai, Maharashtra 400701
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your phone number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your text"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ my-project/src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
+
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
