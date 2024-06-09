663802015bd037efbcc83e4dfb8a90368b47e18e
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2024-06-09 15:05:38 +0000

    fixed card css and apply button

--- my-project/src/components/Cards/Cards.jsx
+++ my-project/src/components/Cards/Cards.jsx
@@ -31,7 +31,7 @@ const Card = ({ title, description, imageSource, id }) => {
             <h1 class="title">{title}</h1>
             <p class="description">{description}</p>
 
-            <button type="button" class="jobcard-apply-button" onclick="handleClick()">
+            <button type="button" class="jobcard-apply-button"  onClick={handleClick}>
               Apply
             </button>
           </div>
