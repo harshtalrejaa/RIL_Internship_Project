91b815add82155f801d0fec1346a231bee63b76e
Khushipuru <111975122+Khushipuru@users.noreply.github.com>
2024-06-09 11:22:00 +0000

    Home page

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Hero.css";
 import dark_arrow from "../../assets/dark-arrow.png";
+import hero_img from "../../assets/Web_Photo_Editor.jpg";
 
 const Hero = () => {
   return (
-    <section id="hero">
     <div className="hero">
       <div className="hero-text">
+        <h1>Unlock Your Potential:</h1>
+        <p>Expert-Led Training for a Brighter Future!</p>
+      </div>
+      <div className="hero-img">
+        <img src={hero_img} alt="" className="hero-img" />
       </div>
     </div>
-    </section>
   );
 };
 
